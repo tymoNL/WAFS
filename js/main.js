aaf1d1053359246841a857dfcad57f8a13ac3ac7
Tymo <tymo.smids@gmail.com>
2025-02-11 13:26:39 +0100

    Verion 3.0
    Added gradient, box shadow, json data.

--- js/main.js
+++ js/main.js
@@ -5,34 +5,55 @@ const endpointMe = 'items/person/225';
 const myURL = baseURL + endpointMe;
 
 getData(myURL).then(dataTymo => {
-    // Pak het h1 element uit de dom
-    let scoreBoardText = document.querySelector(".nameText");
+    //--- Variable zetten uit Json data ---\\
+    // Info variables
+    let jsonData = dataTymo.data.custom;
+    let customData = JSON.parse(jsonData);
+    let myAvatar = dataTymo.data.avatar;
+    let myBio = dataTymo.data.bio;
+    let myName = dataTymo.data.name;
+    let myBirthDate = dataTymo.data.birthdate;
+    let website = dataTymo.data.website;
+    
+    // Favorites variables
+    let myColor = dataTymo.data.fav_color;
+    let myFood = dataTymo.data.fav_kitchen;
+    let myAttribute = dataTymo.data.fav_attribute;
+    let myBook = dataTymo.data.fav_book_genre;
+    let myFeature = dataTymo.data.fav_feature;
+    let myProperty = dataTymo.data.fav_property;
+    let myTag = dataTymo.data.fav_tag;
+    //-------------------------------------\\
 
-    // Variable zetten met de naam uit data
-    let myName = dataTymo.data.name; 
+    console.log("data: ", dataTymo.data);
+    console.log("customdata: ", customData);
 
-    // Verander de tekst in het h1 element
-    scoreBoardText.textContent = myName;
+    //--- Pak elementen uit de DOM ---\\
+    // Firstbase clickArea
+    let colorText = document.querySelector(".firstBaseClickArea .colorText");
+    let foodText = document.querySelector(".firstBaseClickArea .foodText");
+    let attributeText =  document.querySelector(".firstBaseClickArea .attributeText");
+    let bookText = document.querySelector(".firstBaseClickArea .bookText");
+    let featureText = document.querySelector(".firstBaseClickArea .featureText");
+    let propertyText =  document.querySelector(".firstBaseClickArea .propertyText");
+    let tagText =  document.querySelector(".firstBaseClickArea .tagText");
+    
+    // Secondbase clickArea
+    let nameText = document.querySelector(".secondBaseClickArea .nameText");
+    let birthdateText = document.querySelector(".secondBaseClickArea .birthdateText");
+    let image =  document.querySelector(".secondBaseClickArea .avatar");
 
+    // Thirdbase clickArea
+    let hobbys = document.querySelector(".secondBaseClickArea .hobbys");
+    //--------------------------------\\
+
+    // Verander de elementen met de Json data
+    image.src = dataTymo.data.avatar;
+    nameText.textContent = myName;
+    birthdateText.textContent = myBirthDate;
 });
 
-
-async /*9*/ function getData(URL) {
-	return ( //8
-		fetch(URL) //1
-		.then ( //2
-			response /*3*/ => response.json() //4
-		)
-		.then ( //5
-			jsonData /*6*/ => {return jsonData} //7
-		)
-	);
-}
-
-
-window.addEventListener("load", (event) => {
-    EnableClickEvents();
-});
+window.addEventListener("load", (event) => { EnableClickEvents(); });
 
 function EnableClickEvents() {
     // Set all clickable elements
@@ -43,7 +64,7 @@ function EnableClickEvents() {
     window.onclick = e => {
         var clickedElement = e.target;
 
-        console.log(clickedElement);
+        // console.log(clickedElement);
 
         // Check if clicked element is clickable element
         switch (clickedElement) {
@@ -61,4 +82,13 @@ function EnableClickEvents() {
                 break;
         }
     }
+}
+
+// Get Json data
+async function getData(URL) {
+    return (
+        fetch(URL)
+            .then( response => response.json())
+            .then( jsonData => { return jsonData })
+    );
 }
